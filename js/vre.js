177d69f31f008df60366d4191c31d42657dcd680
David Puckett <david@brightrain.com>
2014-09-05 11:58:21 -0600

    update to show map click event to toggle hiding and showing different html elements

--- js/vre.js
+++ js/vre.js
@@ -1,7 +1,12 @@
 (function vre() {
 	var map;
 	$(document).ready(function(){
+        $(".station-info-pane").hide();
         map = L.mapbox.map('map', 'brightrain.j0jhfgf8');
+        map.on("click", function(e) {
+            $(".station-info-pane").hide();
+            $("#alexandria-station-info").show();
+        });
         /*CORS issue
         L.mapbox.featureLayer()
                 .loadURL('http://dev.brightrain.com/vre/data/stations.json')
